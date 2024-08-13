6509f374c654d0105603338f993d49ce4feb66e0
ironchef001 <ironchef001@yahoo.com>
2024-08-13 13:02:43 -0500

    upd

--- python/0121-best-time-to-buy-and-sell-stock.py
+++ python/0121-best-time-to-buy-and-sell-stock.py
@@ -8,3 +8,18 @@ class Solution:
                 lowest = price
             res = max(res, price - lowest)
         return res
+
+
+# 240813: chose not sell on the same day if it's the lowest day
+def maxProfit_me(self, prices: List[int]) -> int:
+        lowest = prices[0]
+        res = 0
+
+        for price in prices:
+            lowest = min(price, lowest)
+            if price < lowest:
+                lowest = price
+            else:
+                res = max(res, price - lowest)
+        
+        return res
